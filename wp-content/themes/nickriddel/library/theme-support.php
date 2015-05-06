988ab942ac06b93c7f5ae113672905b2f3b15643
Trent Newton <trent99878@gmail.com>
2015-05-06 10:12:37 +1000

    removed emoji sources from header

--- wp-content/themes/nickriddel/library/theme-support.php
+++ wp-content/themes/nickriddel/library/theme-support.php
@@ -34,6 +34,11 @@ function show_title($path) {
     echo $title;
 }
 
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
 add_theme_support( 'woocommerce' );
 
 add_action('after_setup_theme', 'FoundationPress_theme_support'); 
