e114f723d6b99a1e317c60c5de33d22658ba52c5
Trent Newton <trent99878@gmail.com>
2015-05-19 11:44:46 +1000

    fixed margin-top issue with admin bar

--- wp-content/themes/nickriddel/library/enqueue-stylesheets.php
+++ wp-content/themes/nickriddel/library/enqueue-stylesheets.php
@@ -20,6 +20,30 @@ function mgt_dequeue_stylesandscripts() {
         wp_dequeue_style( 'select2' );
         wp_deregister_style( 'select2' );
     } 
-} 
+}
+
+add_action('get_header', 'remove_admin_login_header');
+function remove_admin_login_header() {
+	remove_action('wp_head', '_admin_bar_bump_cb');
+}
+
+add_action('wp_head', 'add_header_styles');
+
+function add_header_styles() {
+  if ( is_admin_bar_showing() ) {?>
+    <style>
+	html { margin-top: 32px; }
+	* html body { margin-top: 32px; }
+	@media screen and ( max-width: 782px ) {
+		html { margin-top: 46px; }
+		* html body { margin-top: 46px; }
+	}
+	@media screen and ( max-width: 600px ) {
+	html { margin-top: 0; }
+	* html body { margin-top: 0; }
+	}
+    </style>
+  <?php }
+}
 
 ?>
