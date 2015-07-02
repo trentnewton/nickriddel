e1e15803d2a2c1c33aad6e08ac930f510aa5046a
Trent Newton <trent99878@gmail.com>
2015-07-02 21:38:55 +1000

    implemented a better sticky footer

--- wp-content/themes/nickriddel/footer.php
+++ wp-content/themes/nickriddel/footer.php
@@ -1,6 +1,6 @@
 	</div>
 	<footer>
-      <div class="row">
+      <div class="inside-footer row">
         <div class="small-12 columns">
           <p class="copyright">
           	&copy;&nbsp;<span class="year"><?php echo date("Y"); ?></span>&nbsp;<?php bloginfo( 'name' ); ?><span class="divider"></span><a href="<?php echo home_url(); ?>/cart">Cart</a>
