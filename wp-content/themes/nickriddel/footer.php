fb456d353352747fe22a7f8968749c99edcd8cb4
Trent Newton <trent99878@gmail.com>
2015-07-19 04:17:26 +1000

    made album img 100%; fixed modal issues; made the loading screen fixed

--- wp-content/themes/nickriddel/footer.php
+++ wp-content/themes/nickriddel/footer.php
@@ -1,13 +1,13 @@
-	</div>
-	<footer>
-      <div class="inside-footer row">
-        <div class="small-12 columns">
-          <p class="copyright">
-          	&copy;&nbsp;<span class="year"><?php echo date("Y"); ?></span>&nbsp;<?php bloginfo( 'name' ); ?><span class="divider"></span><a href="<?php echo home_url(); ?>/cart">Cart</a>
-          </p>
-        </div>
+  </div>
+  <footer>
+    <div class="inside-footer row">
+      <div class="small-12 columns">
+        <p class="copyright">
+        	&copy;&nbsp;<span class="year"><?php echo date("Y"); ?></span>&nbsp;<?php bloginfo( 'name' ); ?><span class="divider"></span><a href="<?php echo home_url(); ?>/cart">Cart</a>
+        </p>
       </div>
-    </footer>
+    </div>
+  </footer>
 	<?php wp_footer(); ?>
 </body>
 </html>
